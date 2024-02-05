2c1d8d9d71a62e390edde0e81dbe5c51369f31bc
LordAx <azrai.mok@gmail.com>
2024-02-05 17:31:54 +0800

    sd

--- pipeline.py
+++ pipeline.py
@@ -14,8 +14,8 @@ nltk.download('wordnet')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
-# from nltk.corpus import stopwords
-# stop_words = set(stopwords.words('english'))
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
 from nltk.tokenize import word_tokenize
 
 
